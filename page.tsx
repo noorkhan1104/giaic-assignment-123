1099d8ec200b4306abe534073626deb09284e8ac
noorkhan1104 <164567175+noorkhan1104@users.noreply.github.com>
2024-10-27 15:45:13 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,32 @@
+// import Image from "next/image";
+ import Link from "next/link";
+import Header from "./components/Header";
+// import Footer from "./components/Footer/Footer";
+// import Header from "./components/header/Header";
+// import Navbar from "./components/Navbar/Navbar";
+
+
+// export default function Home (){
+//   return(
+//     <div>
+//   <Navbar/>
+//     <ul>
+
+//       <Header/>
+//       <br />
+
+//       <Footer/>
+//     </ul>
+
+//     </div>
+//   )
+// }
+
+
+export default function Home()  {
+  return (
+    <div>
+       <Header/>
+    </div>
+  )
+}
